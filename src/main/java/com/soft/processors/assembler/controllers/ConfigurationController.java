d53ff70aa87f29e0442f5e7f8c4ae7d66ef75cd1
Kalin Ivanov <kalin2578@gmail.com>
2023-06-22 19:21:52 +0300

    minor update

--- src/main/java/com/soft/processors/assembler/controllers/ConfigurationController.java
+++ src/main/java/com/soft/processors/assembler/controllers/ConfigurationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author kalin
  */
 @RestController
-@RequestMapping("config")
+@RequestMapping("/config")
 public class ConfigurationController {
     private final Configuration configuration;
 
