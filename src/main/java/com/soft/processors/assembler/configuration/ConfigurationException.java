4e1217de007fb348b54cc9e576bf7e438dfdc4c1
Kalin Ivanov <kalin2578@gmail.com>
2023-07-12 11:13:53 +0300

    added custom exception

---
+++ src/main/java/com/soft/processors/assembler/configuration/ConfigurationException.java
@@ -0,0 +1,18 @@
+package com.soft.processors.assembler.configuration;
+
+/**
+ * The ConfigurationException defines an Exception throws when a configuration file is wrong.
+ *
+ * @author kalin
+ */
+public class ConfigurationException extends Exception {
+
+  /**
+   * Constructor ConfigurationException.
+   *
+   * @param message the description message for exception
+   */
+  public ConfigurationException(String message) {
+    super(message);
+  }
+}
