3179cd1abfc658adf0149f50fc8d8a82952ac91a
Kalin Ivanov <kalin2578@gmail.com>
2023-07-14 16:59:25 +0300

    added tests

---
+++ src/test/java/com/soft/processors/assembler/ConfigurationTest.java
@@ -0,0 +1,84 @@
+package com.soft.processors.assembler;
+
+import com.google.gson.JsonSyntaxException;
+import com.soft.processors.assembler.configuration.Configuration;
+import com.soft.processors.assembler.configuration.ConfigurationException;
+import com.soft.processors.assembler.configuration.InstructionConfig;
+import com.soft.processors.assembler.configuration.InstructionFieldsConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = Configuration.class)
+class ConfigurationTest {
+  private Configuration configuration;
+
+  @BeforeEach
+  void setUp() {
+    configuration = new Configuration();
+  }
+
+  @Test
+  void readConfig() throws ConfigurationException {
+    configuration.readConfig();
+
+    InstructionFieldsConfig instructionFieldsConfig =
+            configuration.getInstructionFieldsConfig();
+    assertEquals(3, instructionFieldsConfig.opcodeFieldLength);
+    assertEquals(1, instructionFieldsConfig.addressingModeFieldLength);
+    assertEquals(8, instructionFieldsConfig.operandFieldLength);
+
+    HashMap<String, InstructionConfig> instructionConfigMap =
+            configuration.getInstructionConfigMap();
+    assertNotNull(instructionConfigMap);
+    assertEquals(7, instructionConfigMap.size());
+  }
+
+  @Test
+  void loadDefaultConfig() {
+    configuration.loadDefaultConfig();
+
+    InstructionFieldsConfig instructionFieldsConfig =
+            configuration.getInstructionFieldsConfig();
+    assertEquals(3, instructionFieldsConfig.opcodeFieldLength);
+    assertEquals(1, instructionFieldsConfig.addressingModeFieldLength);
+    assertEquals(8, instructionFieldsConfig.operandFieldLength);
+
+    HashMap<String, InstructionConfig> instructionConfigMap =
+            configuration.getInstructionConfigMap();
+    assertNotNull(instructionConfigMap);
+    assertEquals(0, instructionConfigMap.size());
+  }
+
+  @Test
+  void readConfigurationWithInvalidFile() {
+    Configuration.setCfgFILE("invalid_config.json");
+    assertThrows(ConfigurationException.class, () -> configuration.readConfig());
+  }
+  @Test
+  void readConfigurationWithInvalidJson() throws IOException {
+    File tempFile = File.createTempFile("temp_config", ".json");
+    FileWriter writer = new FileWriter(tempFile);
+    writer.write("{invalid_json}");
+    writer.close();
+
+    Configuration.setCfgFILE(tempFile.getPath());
+
+    assertThrows(JsonSyntaxException.class, configuration::readConfig);
+  }
+
+  @Test
+  void getInstructionConfigWithInvalidMnemonic() {
+    assertThrows(JsonSyntaxException.class, () -> configuration.readConfig());
+    InstructionConfig instructionConfig = configuration.getInstructionConfig("INVALID_MNEMONIC");
+
+    assertNull(instructionConfig);
+  }
+}
