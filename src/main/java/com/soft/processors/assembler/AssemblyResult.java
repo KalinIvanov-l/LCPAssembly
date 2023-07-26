a427e07ea8b6e7379db846ec274771abaa2ecacb
Kalin Ivanov <kalin2578@gmail.com>
2023-07-26 21:37:48 +0300

    minor update

--- src/main/java/com/soft/processors/assembler/AssemblyResult.java
+++ src/main/java/com/soft/processors/assembler/AssemblyResult.java
@@ -4,6 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * This AssemblyResult class provides listing from given instruction.
+ *
+ * @author kalin
+ */
 @Getter
 @Setter
 @AllArgsConstructor
