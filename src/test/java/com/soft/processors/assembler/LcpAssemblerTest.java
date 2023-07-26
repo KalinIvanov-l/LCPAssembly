e5e52fb2542745e684eab237e5f24468c6f340d2
Kalin Ivanov <kalin2578@gmail.com>
2023-07-26 19:59:35 +0300

    added tests

--- src/test/java/com/soft/processors/assembler/LcpAssemblerTest.java
+++ src/test/java/com/soft/processors/assembler/LcpAssemblerTest.java
@@ -1,34 +1,34 @@
-//package com.soft.processors.assembler;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import java.io.IOException;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class LcpAssemblerTest {
-//  private static final String TEST_FILE = "src/test/resources/test.txt";
-//
-//  @Test
-//  void checkParseSourceFileWithExistFile() throws IOException {
-//    boolean result = LcpAssembler.parseSourceFile(TEST_FILE);
-//
-//    assertTrue(result);
-//    assertEquals(7, LcpAssembler.getSYMBOL_TABLE().getSymbolStore().size());
-//    assertEquals(22, LcpAssembler.getPROGRAM().size());
-//  }
-//
-//  @Test
-//  void checkParseSourceFileWithNotExistFile() {
-//    assertThrows(IOException.class, () -> LcpAssembler.parseSourceFile("src/test/resources/invalid_program.txt"));
-//  }
-//
-//  @Test
-//  void checkAssembleOutput() throws IOException {
-//    AssemblyResult result = LcpAssembler.assemble(TEST_FILE);
-//
-//    assertNotNull(result);
-//    assertNotNull(result.getListing());
-//    assertNotNull(result.getOutputFile());
-//  }
-//}
+package com.soft.processors.assembler;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LcpAssemblerTest {
+  private static final String TEST_FILE = "src/test/resources/test.txt";
+
+  @Test
+  void checkParseSourceFileWithExistFile() throws IOException {
+    boolean result = LcpAssembler.parseSourceFile(TEST_FILE);
+
+    assertTrue(result);
+    assertEquals(7, LcpAssembler.getSYMBOL_TABLE().getSymbolStore().size());
+    assertEquals(22, LcpAssembler.getPROGRAM().size());
+  }
+
+  @Test
+  void checkParseSourceFileWithNotExistFile() {
+    assertThrows(IOException.class, () -> LcpAssembler.parseSourceFile("src/test/resources/invalid_program.txt"));
+  }
+
+  @Test
+  void checkAssembleOutput() throws IOException {
+    AssemblyResult result = LcpAssembler.assemble(TEST_FILE);
+
+    assertNotNull(result);
+    assertNotNull(result.getListing());
+    assertNotNull(result.getOutputFile());
+  }
+}
