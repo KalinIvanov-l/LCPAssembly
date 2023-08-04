d0a6a30514ba6adf4cb51e4d77076080e6fd32e4
Kalin Ivanov <kalin2578@gmail.com>
2023-08-04 11:55:31 +0300

    added simple tests

---
+++ src/test/java/com/soft/processors/assembler/LcpAssemblerTest.java
@@ -0,0 +1,28 @@
+package com.soft.processors.assembler;
+
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class LcpAssemblerTest {
+  @Test
+  void checkStringFile() {
+    assertThrows(IllegalArgumentException.class, () -> LcpAssembler.assemble(""));
+  }
+
+  @Test
+  void assembleWithValidProgramFile() throws IOException {
+    String fileName = "src/main/resources/gcd_a.asm";
+    AssemblyResult result = LcpAssembler.assemble(fileName);
+
+    assertTrue(result.getOutputFile().endsWith(".lst"));
+  }
+
+  @Test
+  void assembleWithWrongFileName() {
+    String fileName = "invalid.tss";
+    assertThrows(IllegalArgumentException.class, () -> LcpAssembler.assemble(fileName));
+  }
+}
