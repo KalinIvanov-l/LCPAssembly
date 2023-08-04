4e0fabe48a4132b1bdad60b70413af57a4977fb8
Kalin Ivanov <kalin2578@gmail.com>
2023-08-04 12:38:33 +0300

    remove unused test

--- src/test/java/com/soft/processors/assembler/LcpAssemblerTest.java
+++ src/test/java/com/soft/processors/assembler/LcpAssemblerTest.java
@@ -1,31 +1,14 @@
 package com.soft.processors.assembler;
 
-import com.google.gson.JsonSyntaxException;
 import org.junit.jupiter.api.Test;
-import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 class LcpAssemblerTest {
   @Test
   void checkWithEmptyStringFile() {
     assertThrows(IllegalArgumentException.class, () -> LcpAssembler.assemble(""));
   }
-
-//  @Test
-//  void assembleWithValidProgramFile() throws IOException {
-//    String fileName = "src/main/resources/gcd_a.asm";
-//    AssemblyResult result;
-//
-//    try {
-//      result = LcpAssembler.assemble(fileName);
-//      assertTrue(result.getOutputFile().endsWith(".lst"));
-//    } catch (JsonSyntaxException e) {
-//      fail("JSON syntax error: " + e.getMessage());
-//    }
-//  }
 
   @Test
   void assembleWithWrongFileName() {
