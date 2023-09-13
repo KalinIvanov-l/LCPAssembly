2a286477c15b3f39e2eefd10d6e203eeb9ac5cf2
Kalin Ivanov <kalin2578@gmail.com>
2023-09-13 11:57:22 +0300

    add react buddy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import {ChakraProvider} from "@chakra-ui/react";
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>
+    <ChakraProvider>
+        <DevSupport ComponentPreviews={ComponentPreviews}
+                    useInitialHook={useInitial}
+        >
+            <App/>
+        </DevSupport>
+    </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
