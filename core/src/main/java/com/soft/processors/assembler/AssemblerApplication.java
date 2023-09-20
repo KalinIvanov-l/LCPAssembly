b3cd1e0528cb3002144b992444a0c01e454958c1
Kalin Ivanov <kalin2578@gmail.com>
2023-09-20 11:42:41 +0300

    remove unused exception

--- core/src/main/java/com/soft/processors/assembler/AssemblerApplication.java
+++ core/src/main/java/com/soft/processors/assembler/AssemblerApplication.java
@@ -1,6 +1,5 @@
 package com.soft.processors.assembler;
 
-import com.soft.processors.assembler.configuration.ConfigurationException;
 import java.io.IOException;
 import java.nio.file.Path;
 import org.springframework.boot.SpringApplication;
@@ -13,7 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class AssemblerApplication {
-  public static void main(String[] args) throws IOException, ConfigurationException {
+  public static void main(String[] args) throws IOException {
     SpringApplication.run(AssemblerApplication.class, args);
     LcpAssembler.assemble(Path.of("core/src/main/resources/test.txt"));
   }
