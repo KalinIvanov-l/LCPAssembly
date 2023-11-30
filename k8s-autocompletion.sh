69780c3605c700c7b335eba30bc61deae80134ac
Kalin Ivanov <kalin2578@gmail.com>
2023-11-30 11:47:28 +0200

    tweak some experiments with k8s

--- k8s-autocompletion.sh
+++ k8s-autocompletion.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "source <(kubectl completion bash)" >>~/.bashrc # add autocomplete permaneently to your bash shell.
+# shellcheck disable=SC2129
+echo "source <(kubectl completion bash)" >>~/.bashrc # add autocomplete permanently to your bash shell.
 echo 'alias k=kubectl' >>~/.bashrc
 echo 'complete -o default -F __start_kubectl k' >>~/.bashrc
+# shellcheck disable=SC1090
 source ~/.bashrc
