37a5b6d52418302cd70e9f7d8e6b57b41aece166
Kalin Ivanov <kalin2578@gmail.com>
2024-02-15 20:47:46 +0200

    Add Data lombok annotation

--- core/src/main/java/com/soft/processors/assembler/configuration/InstructionConfig.java
+++ core/src/main/java/com/soft/processors/assembler/configuration/InstructionConfig.java
@@ -1,19 +1,13 @@
 package com.soft.processors.assembler.configuration;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Data;
 
 /**
  * This class represents the configuration of an instruction in an assembler.
  *
  * @author kalin
  */
-@Getter
-@Setter
-@NoArgsConstructor
-@ToString
+@Data
 public class InstructionConfig {
   public String mnemocode;
   public int opcode;
--- core/src/main/java/com/soft/processors/assembler/configuration/InstructionFieldsConfig.java
+++ core/src/main/java/com/soft/processors/assembler/configuration/InstructionFieldsConfig.java
@@ -1,19 +1,13 @@
 package com.soft.processors.assembler.configuration;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Data;
 
 /**
  * This class represents the configuration of the fields in an instruction in an assembler.
  *
  * @author kalin
  */
-@Getter
-@Setter
-@NoArgsConstructor
-@ToString
+@Data
 public class InstructionFieldsConfig {
   public int opcodeFieldLength;
   public int addressingModeFieldLength;
